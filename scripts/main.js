7573093c059dd65a7d7308d4eae41418507d654e
billyndroid <70548591+billyndroid@users.noreply.github.com>
2021-12-28 20:08:40 +0000

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,18 @@
 
-let n = prompt("Type a number here!");
-function add7(sevenAdded) {
-    sevenAdded = parseInt(n) + 7;
-    return(sevenAdded);
+// function that adds seven to a number inputed by user
+//let n = prompt("Type a number here!");
+//function add7(sevenAdded) {
+//    sevenAdded = parseInt(n) + 7;
+//    return(sevenAdded);
+//}
+
+//console.log(add7(n));
+
+// function that multiplies 2 random numbers
+num1 = Math.floor(Math.random() * 100);
+num2 = Math.floor(Math.random() * 10);
+
+function multiply(num1, num2) {
+    return num1 * num2;
 }
-console.log(add7(n));
+console.log(multiply);
